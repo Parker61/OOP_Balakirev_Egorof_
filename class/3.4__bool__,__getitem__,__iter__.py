7b551326bb3e604c57cae92da438b11aec44bdc0
Parker61 <kovalchukpv61@gmail.com>
2023-06-18 20:34:34 +0300

    18/06/23

--- class/3.4__bool__,__getitem__,__iter__.py
+++ class/3.4__bool__,__getitem__,__iter__.py
@@ -5021,8 +5021,9 @@ class TicTacToe:
             i, j = map(int, input('Enter index: ').split())
             if not (0 <= i < self.size) or not (0 <= j < self.size):
                 continue
-            if self[i, j] == self.FREE_CELL:  # отправляет в setitem
-                self[i, j] = self.HUMAN_X
+            # self.pole[y][x].value можно заменить на просто self[y, x], для этого и  __setitem__ создавали)
+            if self[i, j] == self.FREE_CELL:  # отправляет в getitem
+                self[i, j] = self.HUMAN_X  # отправляет в setitem
                 break
 
     def computer_go(self):
@@ -5060,11 +5061,11 @@ class TicTacToe:
                 return
         # проверка на выигрыш по диагоналям человека
         if all(self.pole[i][i].value == self.HUMAN_X for i in range(self.size)) \
-                or all(self.pole[i][-1-i].value == self.HUMAN_X for i in range(self.size)):
+                or all(self.pole[i][-1 - i].value == self.HUMAN_X for i in range(self.size)):
             self.win = 1
             return
         if all(self.pole[i][i].value == self.COMPUTER_O for i in range(self.size)) \
-                or all(self.pole[i][-1-i].value == self.COMPUTER_O for i in range(self.size)):
+                or all(self.pole[i][-1 - i].value == self.COMPUTER_O for i in range(self.size)):
             self.win = 2
             return
         # проверка на ничью
@@ -5097,7 +5098,225 @@ class TicTacToe:
 
     def __bool__(self):
         return self.win == 0 and self.win not in (1, 2, 3)
+
+
 ########################################################################
+class TicTacToe:
+    FREE_CELL = 0  # свободная клетка
+    HUMAN_X = 1  # крестик (игрок - человек)
+    COMPUTER_O = 2  # нолик (игрок - компьютер)
+
+    def __init__(self):
+        '''Инициализация'''
+        self.pole = [[Cell() for _ in range(3)] for _ in range(3)]
+        self.free_cells = 9
+        self.winner = None
+        self.diagonals = [[self.pole[i][i] for i in range(3)], [self.pole[-i - 1][i] for i in range(-3, 0)]]
+        # Список self.diagonals содержит все возможные диагонали на игровом поле, включая главную диагональ и все
+        # побочные диагонали. Для каждой диагонали список элементов заполняется по порядку, начиная с верхнего левого
+        # угла игрового поля и заканчивая нижним правым углом.
+        for diag in self.diagonals:
+            for cell in diag:
+                cell._priority += 2
+
+    def __getitem__(self, coords):
+        '''Геттер значения клетки по координатам'''
+        if self._valid_coords(*coords):
+            return self.pole[coords[0]][coords[1]].value
+        raise IndexError('некорректно указанные индексы')
+
+    def __setitem__(self, coords, value):
+        '''Сеттер значения клетки по координатам'''
+        if not self._valid_coords(*coords):
+            raise IndexError('некорректно указанные индексы')
+        if value in (0, 1, 2):
+            self.pole[coords[0]][coords[1]].value = value
+        self.__bool__()
+
+    def __bool__(self):
+        '''Проверка окончена игра или нет'''
+        for row in self.pole:  # перебор по строкам
+            if not any(row) and len(set(row)) == 1:
+                # any(row) xотя бы один элемент в последовательности True,т е. хотя бы один self.value = 0,т.к return
+                # not self.value
+                # not any(row) вернет False обращаясь к def __bool__(self):/ return not self.value когда self.value = 0,
+                # если все элементы row являются ложными. это означает, что если все элементы
+                # в ряду пустые (отсутствуют), то возвращается False, а если в ряду есть хотя бы один непустой элемент
+                # и все элементы имеют одинаковое значение, то возвращается True. len(set(row)) возвращает количество
+                # уникальных значений в строке. Если это значение равно 1, то все элементы в этой строке имеют одно и
+                # то же значение, и выражение вернет True. len(set(row)) == 1 проверяет, что длина множества уникальных
+                # элементов в списке row равна 1, то есть все элементы списка равны между собой. Если оба условия
+                # истинны, то значит в данной строке все ячейки пустые и равны между собой, что соответствует ситуации
+                # "ничья" в игре. Таким образом, если все клетки в первом столбце равны нулю, а в других столбцах есть
+                # свободные клетки, то игра продолжится, и не будет закончена как ничья до тех пор, пока не будут
+                # заполнены все клетки на игровом поле.
+                self.winner = row[0].value  # определяется победитель
+                return False  # был найден победитель, игра заканчивается и дальнейшие проверки уже не нужны.
+        for col in zip(*self.pole):  # транспорнирование матрицы, перебор по столбцам
+            # col будет содержать все элементы из одного столбца игрового поля.
+            if not any(col) and len(set(col)) == 1:
+                self.winner = col[0].value
+                return False
+        for diag in self.diagonals:
+            # Каждый элемент списка diag представляет собой объект Cell
+            if not any(diag) and len(set(diag)) == 1:
+                self.winner = diag[0].value
+                return False
+        if self.free_cells == 0:
+            self.winner = 0
+            return False
+        return True  # продолжение игры
+
+    @property
+    def is_human_win(self):
+        return self.winner == self.HUMAN_X
+
+    @property
+    def is_computer_win(self):
+        return self.winner == self.COMPUTER_O
+
+    @property
+    def is_draw(self):
+        return self.winner == 0
+
+    @classmethod
+    def _valid_coords(cls, y, x):
+        '''Проверка координат'''
+        return type(y) == type(x) == int and 0 <= y < 3 and 0 <= x < 3
+
+    def init(self):
+        self.__init__()
+
+    def human_go(self):
+        '''Ход человека'''
+        while True:
+            print('Введите координаты свободной клетки через пробел:')
+            coords = input()
+            if len(coords.split()) != 2:
+                print('Было введено неверное количество координат.')
+                continue
+            try:
+                y, x = map(int, coords.split())
+                if not self._valid_coords(y, x):
+                    print('Введённые координаты вне диапазона поля.')
+                    continue
+            except ValueError:
+                print('Введённые данные не являются координатами.')
+                continue
+            # if self.pole[y][x].value not in [1, 2]:
+            if self.pole[y][x]:  # обращение к клетке а она обращается к __bool__(self): return not self.value  ->
+                # self.value = 0 -> not False -> True
+                # проверять, существует ли объект Cell для ячейки поля (y, x), но не будет проверять её значение.
+                self.pole[y][x].value = self.HUMAN_X
+                self.free_cells -= 1
+                self._compute_priority(y, x)
+                break
+            else:
+                print('Выбранная клетка занята.')
+                continue
+
+    def computer_go(self):
+        '''Ход компьютера'''
+        # собирает все клетки которые не заняты, if cell проверка не занята ли клетка через __bool__
+        free_cells = [(y, x) for y, row in enumerate(self.pole) for x, cell in enumerate(row) if cell]
+        y, x = max(free_cells, key=lambda c: self.pole[c[0]][c[1]]._priority)
+        # координаты свободной ячейки с максимальным значением приоритета.
+        if y == x == 1:  # Реакция на среднюю клетку
+            for cell in (self.pole[0][1], self.pole[1][0], self.pole[1][2], self.pole[2][1]):
+                cell._priority += 2
+        self.free_cells -= 1
+        self.pole[y][x].value = self.COMPUTER_O
+
+    def _compute_priority(self, y, x):
+        '''Просчёт хода компьютера'''
+        # Реакция на ход противника
+        # Метод self._compute_priority(y, x) используется для пересчёта приоритетов доступных ходов для игрока. Он
+        # вызывается после того, как игрок сделал очередной ход в свободную ячейку поля.Для каждой свободной ячейки
+        # поля метод вычисляет её приоритет, основываясь на том, сколько потенциальных выигрышных комбинаций может быть
+        # построено, если игрок сделает ход в эту ячейку. Чем больше потенциальных выигрышных комбинаций можно
+        # построить, тем выше приоритет у этой ячейки.Пересчёт приоритетов доступных ходов позволяет игроку выбирать
+        # более "выгодные" ходы на каждом шаге игры и повышать свои шансы на победу.Кроме того, метод
+        # self._compute_priority(y, x) также запускается один раз в начале игры, чтобы вычислить начальные приоритеты
+        # для всех ячеек поля.
+        for cell in self.pole[y]:
+            cell._priority -= 1
+        for cell in list(zip(*self.pole))[x]:
+            cell._priority -= 1
+        if y == x:
+            for cell in self.diagonals[0]:
+                cell._priority -= 1
+        if y + x == 2:
+            for cell in self.diagonals[1]:
+                cell._priority -= 1
+        # Корректировка опасности
+        for row in self.pole + [list(lst) for lst in zip(*self.pole)] + self.diagonals:
+            counter = [0, 0, 0]
+            for cell in row:
+                counter[cell.value] += 1
+            if counter[1] == 2:
+                for cell in row:
+                    cell._priority = 5
+            if counter[2] == 2:
+                for cell in row:
+                    cell._priority = 6
+
+    def show(self):
+        '''Отрисовка поля'''
+        for row in self.pole:
+            for cell in row:
+                print(cell, end=' ')
+            print()
+        print('* ' * 10)
+
+
+class Cell:
+    def __init__(self):  # содержит значение клетки и приоритет.
+        self.value = 0
+        self._priority = 2
+        # используется для определения приоритета клетки при выполнении каких-либо операций.
+
+    def __bool__(self):
+        return not self.value
+
+    def __str__(self):
+        if self.value == 1:
+            return 'X'
+        if self.value == 2:
+            return '0'
+        return '-'
+
+    # __eq__ при проверке того, занята ли определенная клетка на игровом поле или нет
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __hash__(self):
+        # Хеш-код нужен, чтобы быстро и эффективно сравнивать объекты между собой быстро сравнить их между собой и
+        # выполнить необходимые действия (например, проверить, занята ли определенная клетка на игровом поле или нет).
+        return hash(self.value)
+
+
+game = TicTacToe()
+game.init()
+step_game = 0
+while game:
+    game.show()
+    if step_game % 2 == 0:
+        game.human_go()
+    else:
+        game.computer_go()
+    step_game += 1
+
+print("Stop____________________________________")
+game.show()
+
+if game.is_human_win:
+    print("Поздравляем! Вы победили!")
+elif game.is_computer_win:
+    print("Все получится, со временем")
+    # elif game.is_draw:
+else:
+    print("Ничья.")
+
 
 ########################################################################
 
