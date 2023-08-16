0967f212929ce85d94c8bd76bab2dee2ad6133ee
Parker61 <kovalchukpv61@gmail.com>
2023-08-16 12:40:57 +0300

    19/07/23

--- class/class_4.5_Полиморфизм и абстрактные методы.py
+++ class/class_4.5_Полиморфизм и абстрактные методы.py
@@ -300,6 +300,43 @@ class AbstractClass(ABC):
         pass
 
 
+################################################################
+from abc import ABC, abstractmethod
+
+
+class Database(ABC):
+    @abstractmethod
+    def connect(self): ...
+
+    @abstractmethod
+    def disconnect(self): ...
+
+    @abstractmethod
+    def execute(self): ...
+
+
+class MySQLDatabase(Database):
+    def connect(self):
+        print('Connecting to MySQL database...')
+
+    def disconnect(self):
+        print('Disconnecting from MySQL database...')
+
+    def execute(self, query):
+        print(f"Executing query '{query}' in MySQL database...")
+
+
+class PostgreSQLDatabase(Database):
+    def connect(self):
+        print('Connecting to PostgreSQL database...')
+
+    def disconnect(self):
+        print('Disconnecting from PostgreSQL database...')
+
+    def execute(self, query):
+        print(f"Executing query '{query}' in PostgreSQL database...")
+
+
 ################################################################################################
 # Подвиг 6 (про модуль abc). В языке Python есть еще один распространенный способ объявления абстрактных методов класса
 # через декоратор abstractmethod модуля abc:from abc import ABC, abstractmethodЧтобы корректно работал декоратор
